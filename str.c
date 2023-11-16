72c8ef2bb1deb11e17726d5c7bd70782da4dd78f
Tanvir <2020tanvir1971@gmail.com>
2023-11-16 22:22:23 +0800

    main

--- str.c
+++ str.c
@@ -1,8 +1,19 @@
-#include<stdio.h>
-int main ()
+#include <stdio.h>
+
+struct person
 {
-    printf("tanvir");
-    
+    /* data */
+    double salary;
+    int aga;
+};
+
+int main()
+{
+    struct person person1;
+    person1.salary = 1000;
+    person1.aga = 20;
+    printf("salary = %.2f\n", person1.salary);
+    printf("aga = %d\n", person1.aga);
 
     return 0;
 }
